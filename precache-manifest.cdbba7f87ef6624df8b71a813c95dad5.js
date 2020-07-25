6fc74a4e0181f5680a8af727b1f1f0d4a224205f
Ved Dandekar <veddandekar6@gmail.com>
2020-07-26 02:34:20 +0530

    Updates

---
+++ precache-manifest.cdbba7f87ef6624df8b71a813c95dad5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb9c355dc4e4a756422f07e4286b8802",
+    "url": "/corona-app/index.html"
+  },
+  {
+    "revision": "20f5f7024b890efab6e3",
+    "url": "/corona-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f644267f98c705655c2c",
+    "url": "/corona-app/static/css/main.46ddc316.chunk.css"
+  },
+  {
+    "revision": "20f5f7024b890efab6e3",
+    "url": "/corona-app/static/js/2.1bbadbec.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/corona-app/static/js/2.1bbadbec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f644267f98c705655c2c",
+    "url": "/corona-app/static/js/main.761c851e.chunk.js"
+  },
+  {
+    "revision": "8ba4a8fc04fbe4fc2ace",
+    "url": "/corona-app/static/js/runtime-main.b00f6dba.js"
+  },
+  {
+    "revision": "c8e45de0a4d9c09751263cb72c400a7c",
+    "url": "/corona-app/static/media/covid1.c8e45de0.png"
+  },
+  {
+    "revision": "2e641595a5cdfabbaa2bdef304a79bc9",
+    "url": "/corona-app/static/media/covid2.2e641595.png"
+  }
+]);
